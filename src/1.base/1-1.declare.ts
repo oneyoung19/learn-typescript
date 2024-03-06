9d1d0d3ac4590fa886562a7181ba72a3bb717d41
yangxiaoping <2878847069@qq.com>
2024-03-06 13:44:39 +0800

    feat: 类型声明和类型推断

---
+++ src/1.base/1-1.declare.ts
@@ -0,0 +1,11 @@
+// 类型声明
+// 在变量或者表达式后边加冒号
+
+let age:number = 19
+
+function getName(name:string):string {
+  return name
+}
+
+// 报错
+let num:number = 'Hello world'
---
+++ src/1.base/1-2.infer.ts
@@ -0,0 +1,7 @@
+// 类型推断
+// 好处之一是，能够保证现有没有加入type类型系统的javascript代码平稳运行
+
+let foo = 123
+
+// error
+foo = 'Hello world'
