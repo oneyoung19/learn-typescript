6fc0605d399e1b591046fd23735111d715fc2b24
yangxiaoping <2878847069@qq.com>
2024-03-14 20:53:28 +0800

    feat: interface

---
+++ src/6.object/6-2.interface.ts
@@ -0,0 +1,22 @@
+// type
+type MyObj = {
+  x: number,
+  y: number
+}
+
+const obj:MyObj = {
+  x: 1,
+  y: 2
+}
+
+
+// interface
+interface MyInterface {
+  x: number;
+  y: number;
+}
+
+const myInterface:MyInterface = {
+  x: 1,
+  y: 2
+}
