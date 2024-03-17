b7dbcacd81c254f81fd6b27ce6f0bbcb10bf4846
yangxiaoping <2878847069@qq.com>
2024-03-17 18:41:10 +0800

    feat: 非空断言

---
+++ src/11.assert/11-3.exist.ts
@@ -0,0 +1,12 @@
+// https://wangdoc.com/typescript/assert#%E9%9D%9E%E7%A9%BA%E6%96%AD%E8%A8%80
+
+// 非空断言
+
+// 使用方式是在变量后添加感叹号！
+
+const root = document.getElementById('root')
+
+console.log(root.name)
+// root.addEventListener('click', (e: MouseEvent) => {
+//   /* ... */
+// })
