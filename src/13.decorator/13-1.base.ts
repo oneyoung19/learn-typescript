1c2efb8fabbc49cea30c900aa8966ae9cd809df6
yangxiaoping <2878847069@qq.com>
2024-03-17 19:12:45 +0800

    feat: decorator

---
+++ src/13.decorator/13-1.base.ts
@@ -0,0 +1,12 @@
+// https://wangdoc.com/typescript/decorator
+
+function simpleDecorator(
+  value:any,
+  context:any
+) {
+  console.log(`hi, this is ${context.kind} ${context.name}`)
+  return value;
+}
+
+@simpleDecorator
+class A {} // "hi, this is class A"
